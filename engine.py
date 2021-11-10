526182a6fec6f631a252c6928d5ed8ed35e83e72
A1029384756 <hayden.gray104@gmail.com>
2021-11-09 20:39:34 -0500

    Added world terrain output

--- engine.py
+++ engine.py
@@ -1,7 +1,25 @@
 import os
 
+terrainMappings = {'Giant Mushroom Forest':'gmf', 'Tornado-Ravaged Desert':'d', 'Forest':'f','Bleak Tundra':'t', 'Ancient Jungle':'j'}
+
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def drawWorld():
+def drawWorld(world, player):
+    for i in range(int(world.scale)):
+        for j in range(int(world.scale)):
+            terrain = terrainMappings[world.world_array[i][j]]
+            terrain = textSpacing(terrain, 3)
+            print(terrain, end = " ")
+        print()
     return 0
+
+def textSpacing(text, maxLength):
+    textLength = len(text)
+
+    for i in range(maxLength - textLength):
+        if (i % 2) != 0:
+            text = ' ' + text
+        else:
+            text = text + ' '
+    return text
--- main.py
+++ main.py
@@ -3,50 +3,52 @@ from player import Player
 from world import WorldSpace
 from combat import combatLoop
 
-scale = int(input("Input your desired world scale. "))
+if __name__ == "__main__":
+    scale = int(input("Input your desired world scale. "))
 
-player = Player(int(scale/2), int(scale/2), scale)
-world = WorldSpace(scale)
-world.worldGen()
-world.terrainGen()
-world.enemyGeneration(player)
-world.consumablesGen()
+    player = Player(int(scale/2), int(scale/2), scale)
+    world = WorldSpace(scale)
+    world.worldGen()
+    world.terrainGen()
+    world.enemyGeneration(player)
+    world.consumablesGen()
 
-while True:
-    engine.clearScreen()
-    if player.infected and player.infectedDuration == 0:
-        print("You have succumbed to vampiritis. Game over.")
-        break
-
-    if player.counter > 10:
-        world.enemyGeneration(player)
-        player.counter = 0
-        print("The Blood Moon rises, the enemies have gathered their strength again.")
-
-    w, c = player.getDrops(world)
-    if w != ' ':
-      player.pickupDrops(world, 0)
-    if c != ' ':
-      player.pickupDrops(world, 1)
-
-    if world.getEnemy(player) == ' ':
-        terrain = world.getTerrain(player)
-        print('You are in the ' + terrain + '.')
-        player.decision()
-    else:
-        enemy = world.getEnemy(player)
-        terrain = world.getTerrain(player)
-        print('You are in the ' + terrain + '.')
-        if enemy == "vampire" and world.night == False:
-            player.decision()
-        print('You see a(n) ' + enemy + '.')
-        victory = combatLoop(enemy, player, world)
-        if victory == 1:
-            world.clearEnemy(player)
-            player.pickupDrops(world, 1)
-            player.decision()
-        elif victory == 0:
-            print('Game Over')
+    while True:
+        engine.clearScreen()
+        engine.drawWorld(world, player)
+        if player.infected and player.infectedDuration == 0:
+            print("You have succumbed to vampiritis. Game over.")
             break
-        elif victory == -1:
+
+        if player.counter > 10:
+            world.enemyGeneration(player)
+            player.counter = 0
+            print("The Blood Moon rises, the enemies have gathered their strength again.")
+
+        w, c = player.getDrops(world)
+        if w != ' ':
+          player.pickupDrops(world, 0)
+        if c != ' ':
+          player.pickupDrops(world, 1)
+
+        if world.getEnemy(player) == ' ':
+            terrain = world.getTerrain(player)
+            print('You are in the ' + terrain + '.')
             player.decision()
+        else:
+            enemy = world.getEnemy(player)
+            terrain = world.getTerrain(player)
+            print('You are in the ' + terrain + '.')
+            if enemy == "vampire" and world.night == False:
+                player.decision()
+            print('You see a(n) ' + enemy + '.')
+            victory = combatLoop(enemy, player, world)
+            if victory == 1:
+                world.clearEnemy(player)
+                player.pickupDrops(world, 1)
+                player.decision()
+            elif victory == 0:
+                print('Game Over')
+                break
+            elif victory == -1:
+                player.decision()
